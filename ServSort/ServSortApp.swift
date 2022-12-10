230ebeabcfdc18cdf2260f89416b676c6162263e
Matt Silvey <webmaster@mattsilvey.com>
2022-12-10 13:28:20 -0600

    Initial Commit

---
+++ ServSort/ServSortApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServSortApp.swift
+//  ServSort
+//
+//  Created by Matthew Silvey on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ServSortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
